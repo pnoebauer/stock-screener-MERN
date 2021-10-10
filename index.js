6e30295c042fb21ddcab4309fbd8bb7cffe905ea
Philip Noebauer <pnoebauer>
2021-10-10 08:22:02 +0800

    update

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+// need to
+// npm install --legacy-peer-deps
 
+require('@babel/register');
 require('dotenv').config();
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.get('/', (req, res) => {
-	res.json('This is working');
-});
-
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-	console.log(`App is running on port ${port}`);
-});
+exports = module.exports = require('./src');

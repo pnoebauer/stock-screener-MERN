5b4d3ed6ef4ae31653fed1b958a7b36bf085de3b
Philip Noebauer <pnoebauer>
2021-10-10 10:00:30 +0800

    update

--- build/index.js
+++ build/index.js
@@ -1,47 +1,47 @@
 "use strict";
 
-var _server = _interopRequireDefault(require("./server"));
-
-var _mongodb = require("mongodb");
-
-var _dataDAO = require("./dao/dataDAO");
+var _express = _interopRequireDefault(require("express"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// require('dotenv').config();
-// import express from 'express';
-// const app = express();
-// app.get('/', (req, res) => {
-// 	res.status(200).json({message: 'Welcome to Node.js & Express'});
-// });
-// const port = process.env.PORT || 8000;
-// app.listen(port, () => {
-// 	console.log(`App is running on port ${port}`);
-// });
-// require('@babel/register');
 require('dotenv').config();
 
-// // import DataCtrl from '../src/api/data.controller';
-const port = process.env.PORT || 8000; // console.log('running', process.env.MONGODB_URI);
-
-_mongodb.MongoClient.connect(process.env.MONGODB_URI, {
-  // wtimeout: 25000,
-  // poolSize: 50,
-  useNewUrlParser: true // useUnifiedTopology: true,
-
-}).catch(err => {
-  console.error(err.stack, '-----error stack');
-  process.exit(1);
-}).then(async client => {
-  // console.log(client);
-  await _dataDAO.DataDAO.injectDB(client); // await StockDataDAO.injectDB(client);
-  // await UsersDAO.injectDB(client);
-  // await CommentsDAO.injectDB(client);
-  // 		// DataCtrl.apiGetData();
-
-  _server.default.listen(port, () => {
-    console.log(`listening on port ${port}`);
-  }); // console.log(`listening on port ${port}`);
-
+const app = (0, _express.default)();
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to Node.js & Express'
+  });
 });
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+}); // import app from './server';
+// import {MongoClient} from 'mongodb';
+// require('dotenv').config();
+// import {DataDAO, StockDataDAO} from './dao/dataDAO';
+// // // import DataCtrl from '../src/api/data.controller';
+// const port = process.env.PORT || 8000;
+// // console.log('running', process.env.MONGODB_URI);
+// MongoClient.connect(process.env.MONGODB_URI, {
+// 	// wtimeout: 25000,
+// 	// poolSize: 50,
+// 	useNewUrlParser: true,
+// 	// useUnifiedTopology: true,
+// })
+// 	.catch(err => {
+// 		console.error(err.stack, '-----error stack');
+// 		process.exit(1);
+// 	})
+// 	.then(async client => {
+// 		// console.log(client);
+// 		await DataDAO.injectDB(client);
+// 		// await StockDataDAO.injectDB(client);
+// 		// await UsersDAO.injectDB(client);
+// 		// await CommentsDAO.injectDB(client);
+// 		// 		// DataCtrl.apiGetData();
+// 		app.listen(port, () => {
+// 			console.log(`listening on port ${port}`);
+// 		});
+// 		// console.log(`listening on port ${port}`);
+// 	});
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,53 +1,52 @@
-// require('dotenv').config();
-
-// import express from 'express';
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-// 	res.status(200).json({message: 'Welcome to Node.js & Express'});
-// });
-
-// const port = process.env.PORT || 8000;
-
-// app.listen(port, () => {
-// 	console.log(`App is running on port ${port}`);
-// });
-
-// require('@babel/register');
-import app from './server';
-import {MongoClient} from 'mongodb';
-
 require('dotenv').config();
 
-import {DataDAO, StockDataDAO} from './dao/dataDAO';
-// // import DataCtrl from '../src/api/data.controller';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.status(200).json({message: 'Welcome to Node.js & Express'});
+});
 
 const port = process.env.PORT || 8000;
 
-// console.log('running', process.env.MONGODB_URI);
+app.listen(port, () => {
+	console.log(`App is running on port ${port}`);
+});
 
-MongoClient.connect(process.env.MONGODB_URI, {
-	// wtimeout: 25000,
-	// poolSize: 50,
-	useNewUrlParser: true,
-	// useUnifiedTopology: true,
-})
-	.catch(err => {
-		console.error(err.stack, '-----error stack');
-		process.exit(1);
-	})
-	.then(async client => {
-		// console.log(client);
-		await DataDAO.injectDB(client);
-		// await StockDataDAO.injectDB(client);
-		// await UsersDAO.injectDB(client);
-		// await CommentsDAO.injectDB(client);
+// import app from './server';
+// import {MongoClient} from 'mongodb';
 
-		// 		// DataCtrl.apiGetData();
+// require('dotenv').config();
 
-		app.listen(port, () => {
-			console.log(`listening on port ${port}`);
-		});
-		// console.log(`listening on port ${port}`);
-	});
+// import {DataDAO, StockDataDAO} from './dao/dataDAO';
+// // // import DataCtrl from '../src/api/data.controller';
+
+// const port = process.env.PORT || 8000;
+
+// // console.log('running', process.env.MONGODB_URI);
+
+// MongoClient.connect(process.env.MONGODB_URI, {
+// 	// wtimeout: 25000,
+// 	// poolSize: 50,
+// 	useNewUrlParser: true,
+// 	// useUnifiedTopology: true,
+// })
+// 	.catch(err => {
+// 		console.error(err.stack, '-----error stack');
+// 		process.exit(1);
+// 	})
+// 	.then(async client => {
+// 		// console.log(client);
+// 		await DataDAO.injectDB(client);
+// 		// await StockDataDAO.injectDB(client);
+// 		// await UsersDAO.injectDB(client);
+// 		// await CommentsDAO.injectDB(client);
+
+// 		// 		// DataCtrl.apiGetData();
+
+// 		app.listen(port, () => {
+// 			console.log(`listening on port ${port}`);
+// 		});
+// 		// console.log(`listening on port ${port}`);
+// 	});
